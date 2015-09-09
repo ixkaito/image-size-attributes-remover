c0aff655e82b8954fd8bef38886531d5ffa162d5
Kite <ixkaito@gmail.com>
2015-09-09 10:51:05 +0900

    Change the function name

--- image-size-attributes-remover.php
+++ image-size-attributes-remover.php
@@ -9,14 +9,14 @@ Description: Remove width and height attributes of image tags.
 License:     GPLv2
 */
 
-if ( ! function_exists ( 'kite_remove_image_size_attr' ) ):
+if ( ! function_exists ( 'isar_remove_image_size_attr' ) ):
 
-	function kite_remove_image_size_attr( $html ) {
+	function isar_remove_image_size_attr( $html ) {
 		$html = preg_replace( '/(width|height)="\d*"\s/', "", $html );
 		return $html;
 	}
 
 endif;
 
-add_filter( 'post_thumbnail_html', 'kite_remove_image_size_attr', 10 );
-add_filter( 'image_send_to_editor', 'kite_remove_image_size_attr', 10 );
+add_filter( 'post_thumbnail_html', 'isar_remove_image_size_attr', 10 );
+add_filter( 'image_send_to_editor', 'isar_remove_image_size_attr', 10 );
